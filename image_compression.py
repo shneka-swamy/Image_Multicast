b8d4c79ab65a4c83f78b70aadb0f6481a51a7eba
shneka-swamy <42727169+shneka-swamy@users.noreply.github.com>
2019-11-14 21:07:43 -0800

    Computer Networks. Image Processing part

---
+++ image_compression.py
@@ -0,0 +1,48 @@
+import cv2
+import base64
+
+
+def save(path, image, jpg_quality=None, png_compression=None):
+    if jpg_quality:
+        cv2.imwrite(path, image, [int(cv2.IMWRITE_JPEG_QUALITY), jpg_quality])
+    elif png_compression:
+        cv2.imwrite(path, image, [int(cv2.IMWRITE_PNG_COMPRESSION), png_compression])
+    else:
+        cv2.imwrite(path, image)
+
+
+def main():
+    # img_path = "png_compression.png"
+    img_path = "sample_image.jpg"
+    image = cv2.imread(img_path)
+    cv2.imshow('Original Image', image)
+
+    # Convert the image to a string
+    with open("png_compression.png", "rb") as img:
+        b64string = base64.b64encode(img.read())
+        print(len(b64string))
+
+    # Save the image with lesser quality
+    out_jpeg = "Modified_Jpeg.jpg"
+    save(out_jpeg, image, jpg_quality=10)
+    img_jpeg = cv2.imread(out_jpeg)
+
+    with open("Modified_jpeg.jpg", "rb") as img:
+        b64string = base64.b64encode(img.read())
+        print(len(b64string))
+
+    # Save the image with more compression
+    out_png = "Modified_png.png"
+    save(out_png, image, png_compression=9)
+    img_png = cv2.imread(out_png)
+
+    with open("Modified_png.png", "rb") as img:
+        b64string = base64.b64encode(img.read())
+        print(len(b64string))
+
+    cv2.waitKey(0)
+    cv2.destroyWindow('Original Image')
+
+
+if __name__ == '__main__':
+    main()
